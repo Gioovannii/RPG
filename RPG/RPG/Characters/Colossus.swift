011ff3eb1bb5dba4ca39ff23afa4af69e76222a8
Gaffe Giovanni <gaffejonathan@ymail.com>
2019-10-14 19:09:37 +0800

    Create of Class Character and his character with proprieties and init

---
+++ RPG/RPG/Characters/Colossus.swift
@@ -0,0 +1,9 @@
+//
+//  Colossus.swift
+//  RPG
+//
+//  Created by Jonathan Gaffé on 2019/10/14.
+//  Copyright © 2019 Giovanni Gaffé. All rights reserved.
+//
+
+import Foundation
---
+++ RPG/RPG/Characters/Warrior.swift
@@ -0,0 +1,9 @@
+//
+//  Warrior.swift
+//  RPG
+//
+//  Created by Jonathan Gaffé on 2019/10/14.
+//  Copyright © 2019 Giovanni Gaffé. All rights reserved.
+//
+
+import Foundation
